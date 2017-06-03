78e4edc5354818c9eef31714b94ba233aa7eadfb
Francisco Oliveira <delley.fx@gmail.com>
2017-06-03 18:57:54 -0300

    Add funcao mudar cor tabela mouse over

---
+++ src/js/mudar-cor.js
@@ -0,0 +1,12 @@
+var trs = document.getElementsByTagName('tr');
+percorreArray(trs, function (tr) {
+
+  tr.addEventListener('mouseover', function () {
+    this.setAttribute('bgcolor', 'grey');
+  })
+
+  tr.addEventListener('mouseout', function () {
+    this.setAttribute('bgcolor', 'white');
+  })
+
+})
